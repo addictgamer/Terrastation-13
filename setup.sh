19c0d13aee70678412e0ffdd13107bf08628a9d9
Ciprian <ciliesj@gmail.com>
2013-08-24 21:12:41 -0700

    Automatic update.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-#TODO: Do something like this.
+# Uh...This script is redundant if used from the repository...
+
+if git clone https://github.com/addictgamer/Terrastation-13.git .
+then
+	:
+else
+	echo "Error: Failed to clone." && exit 1
+fi # if git clone https://github.com/addictgamer/Terrastation-13.git .
 
 
-git clone https://github.com/addictgamer/Terrastation-13.git .
-git checkout main-server
-# Dream Daemon command.
+
+if git checkout main-server
+then
+	:
+else
+	echo "Error: Failed to checkout branch main-server." && exit 1
+fi # if git checkout main-server
--- update.sh
+++ update.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
 #TODO: Do something like this.
-git add -A
-git commit -a -m "Automatic update."
-git push
+
+
+if git add -A
+then
+	:
+else
+	echo "Error: Failed to add all files." && exit 1
+fi # if git add -A
+
+
+if git commit -a -m "Automatic update."
+then
+	:
+else
+	echo "Error: Failed to commit." && exit 1
+fi # if git commit -a -m "Automatic update."
+
+
+if git push
+then
+	:
+else
+	echo "Error: Failed to push to remote." && exit 1
+fi #if git push
+
 # merge command to pull branch master into the main-server repo
-DreamDaemon <port> -trusted
+if git merge overhaul
+then
+	:
+else
+	echo "Error: Failed to merge with branch overhaul."
+fi # if git merge overhaul
